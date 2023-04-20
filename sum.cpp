dc62247aaddcd7e93905757eeb14213e8cf4392a
Luigi Pupillo <pupilloluigi@gmail.com>
2023-04-20 16:38:06 +0200

    Functions SUM and SUBTRACTION completed

--- sum.cpp
+++ sum.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 using namespace std;
-<<<<<<< HEAD
 int sum (int a,int b)// prototype and Declaration Function SUM
 {
 int r;
 r=a+b;
 return r;
 }
+
 int dif (int e,int f)// prototype and Declaration Function DIFFERENCE
 {
 int d;
@@ -18,8 +18,8 @@ int main ()
 {
     int c,m;
     c= sum (5,3);
-    cout <<" The SUM is "<<c<<endl;
+    cout <<"The SUM is "<<c<<endl;
     m=dif(9,2);
-    cout <<"The Difference between the two numbers is "<<m<<endl;
+    cout <<"The Subtraction between the two numbers is "<<m<<endl;
     return 0;
 }   
