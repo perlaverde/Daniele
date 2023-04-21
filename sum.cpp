b493eb8a3352b80762a02e21294e91eb41262b95
Daniele <daniele.angioletti@stellantis.com>
2023-04-21 11:57:07 +0200

    Multiplication function added

--- sum.cpp
+++ sum.cpp
@@ -14,13 +14,23 @@ d = e - f;
 return d;
 }
 
+int multi (int g,int h)// prototype and Declaration Function MULTIPLIER
+{
+int m;
+ m = g * h;
+return m;
+}
+
+
 int main ()
 {
-    int c,m;
+    int c,m,mu;
     c= sum (5,3);
     cout <<"The SUM is "<<c<<endl;
     m=dif(9,2);
     cout <<"The Subtraction between the two numbers is "<<m<<endl;
+    mu=multi(2,3);
+    cout <<"The multiplication between the two numbers is "<<mu<<endl;
     return 0;
 }   
 //Verifica commando PUSH
